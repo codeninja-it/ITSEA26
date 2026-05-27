a9305d9ef8b91898de8764e6a3c2146035f72fd4
Daniele Simoncini <simoncini@codeninja.it>
2026-05-27 15:04:27 +0200

    suddivisione in classi

--- App06/Program.cs
+++ App06/Program.cs
@@ -1,30 +1,15 @@
-﻿// creo una mia funzione riutilizzabile
-string Cifra(string daCifrare, int quantoSpostare) {
-    // e lo voglio cifrare
-    string testoCifrato = "";
-    // scorrendo ogni lettera del mio testo
-    for (int i = 0; i < daCifrare.Length; i++)
-    {
-        // posso recuperare ogni carattere
-        char lettera = daCifrare[i];
-        // convertirlo a numero (su base ASCII)
-        int numero = (int)lettera;
-        // e spostarlo
-        numero = numero + quantoSpostare;
-        // per poi appenderlo nel nuovo testo
-        testoCifrato = testoCifrato + (char)numero;
-    }
-    return testoCifrato;
-}
+﻿using App06;
+
+Utili mieFunzioni = new Utili();
 
 // chiedo all'utente il testo da convertire
 Console.WriteLine("Testo da cifrare:");
 string testo = Console.ReadLine();
 // lo passo alla mia funzione
-string cifrato = Cifra(testo, 150);
+string cifrato = mieFunzioni.Cifra(testo, 150);
 // ne mostro il risultato
 Console.Write(cifrato);
 Console.Write( (char)13 );
 // e lo decifro
-string decifrato = Cifra(cifrato, -150);
+string decifrato = mieFunzioni.Cifra(cifrato, -150);
 Console.WriteLine(decifrato);
---
+++ App06/Utili.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App06
+{
+    internal class Utili
+    {
+        // creo una mia funzione riutilizzabile
+        internal string Cifra(string daCifrare, int quantoSpostare)
+        {
+            // e lo voglio cifrare
+            string testoCifrato = "";
+            // scorrendo ogni lettera del mio testo
+            for (int i = 0; i < daCifrare.Length; i++)
+            {
+                // posso recuperare ogni carattere
+                char lettera = daCifrare[i];
+                // convertirlo a numero (su base ASCII)
+                int numero = (int)lettera;
+                // e spostarlo
+                numero = numero + quantoSpostare;
+                // per poi appenderlo nel nuovo testo
+                testoCifrato = testoCifrato + (char)numero;
+            }
+            return testoCifrato;
+        }
+    }
+}
